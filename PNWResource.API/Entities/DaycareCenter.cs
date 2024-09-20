83f2c2f0fe8ba00e4a542d6a21e480b6e4401e2a
johnweaver <johnweaver@zohomail.com>
2024-09-19 18:01:23 -0700

    added daycare center entity

--- PNWResource.API/Entities/DaycareCenter.cs
+++ PNWResource.API/Entities/DaycareCenter.cs
@@ -2,5 +2,15 @@
 {
     public class DaycareCenter
     {
+        public int Id { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
+        public SchoolType SchoolType { get; set; } = SchoolType.Daycare;
+
+        // Foreign Key
+        public int? CityId { get; set; }
+
+        // Navigation property
+        public City? City { get; set; }
     }
 }
