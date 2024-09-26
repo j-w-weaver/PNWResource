7f76bde0b16248009454e25a387f6eb64e6ba703
johnweaver <johnweaver@zohomail.com>
2024-09-25 17:18:09 -0700

    injected IConfiguration to cloud email service

--- PNWResource.API/Services/CloudMailService.cs
+++ PNWResource.API/Services/CloudMailService.cs
@@ -5,6 +5,12 @@
         private string mail_To = "admin@pnwresource.com";
         private string mail_From = "noreply@pnwresource.com";
 
+        public CloudMailService(IConfiguration configuration)
+        {
+            mail_To = configuration["mailSettings:mailToEmail"]!;
+            mail_From = configuration["mailSettings:mailFromEmail"]!;
+        }
+
         public void SendMail(string subject, string message)
         {
             // send mail
